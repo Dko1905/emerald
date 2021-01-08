407d9c8997933979d0b52ed568179edbf4bc250b
Daniel Florescu <Daniel.florescu1905@protonmail.ch>
2021-01-08 18:41:07 +0100

    Added ssize_t

--- include/kernel/types.h
+++ include/kernel/types.h
@@ -49,7 +49,10 @@ typedef __UINT64_TYPE__ uint64_t;
 #endif
 
 typedef uint64_t size_t;
+typedef int64_t ssize_t;
+
 typedef size_t uintptr_t;
+typedef ssize_t intptr_t;
 
 #ifndef NULL
 #define NULL (void *)0
